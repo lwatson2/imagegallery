870c350477018d0a09700899c480dfe4c9b516e0
lwatson2 <watson.logan@yahoo.com>
2019-01-20 10:26:10 -0600

    Deploy

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ app.use("/image", images);
 
 app.use("/user", users);
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+  //Set static folder
+  app.use(express.static('/client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(config.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
 });
