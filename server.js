e25fb380f755e2add44c3b74b972cc9ce9883140
lwatson2 <watson.logan@yahoo.com>
2019-01-20 11:49:26 -0600

    Testing some stuff

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use("/user", users);
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("/client/build"));
+  app.use("/static", express.static(path.join(__dirname, "client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
