06cc46592ad69e1513136a05bd2dfcbba34b0af2
lwatson2 <watson.logan@yahoo.com>
2019-01-20 11:00:42 -0600

    Changed server js file and gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ENV: process.env.NODE_ENV || "development",
-  MONGODB_URI:
-    process.env.MONGODB_URI ||
-    "mongodb://Logan:logan1@ds211724.mlab.com:11724/image_gallery",
-  PORT: process.env.PORT || 5000,
-  PASS: "rockyisagoat"
-};
