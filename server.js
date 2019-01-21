f171354063012eba92f5c0bd639e31a3e1d88afd
lwatson2 <watson.logan@yahoo.com>
2019-01-21 11:02:41 -0600

    Changed server and package file

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use("/static", express.static(path.join(__dirname, "client/build")));
 
-  app.get("/", function(req, res) {
+  app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
